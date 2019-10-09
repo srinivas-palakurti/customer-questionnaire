9aab709f418da253ae698b39229424ed2abb22cf
srinivas_palakurti <srinivas.palakurti@gmail.com>
2019-10-09 06:33:09 +0530

    Routing added to questionnaire component

--- app/router.js
+++ app/router.js
@@ -3,11 +3,19 @@ import config from './config/environment';
 
 const Router = EmberRouter.extend({
   location: config.locationType,
-  rootURL: config.rootURL
+  rootURL: config.rootURL,
+  // redirct: function () {
+  //   this.transitionTo('home');
+  // }
 });
 
 Router.map(function() {
+  // this.route('home', { path: '/index' });
+  // this.route('/', { path: '/index' });
+
+
   this.route('question-content');
+
 });
 
 export default Router;
